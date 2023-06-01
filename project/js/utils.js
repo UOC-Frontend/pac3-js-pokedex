93fe5e9cec50f6d8e79e605ecb20ada5817d5a33
Elena Piaggio <elenapiaggio@gmail.com>
2023-06-01 09:52:52 +0200

    add addZerosBeforeNumber function

--- project/js/utils.js
+++ project/js/utils.js
@@ -1,3 +1,14 @@
 function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
+
+function addZerosBeforeRandomNumber(number) {
+  if (number < 10) {
+    console.log();
+    return `00${number}`;
+  }
+  if (number < 90) {
+    return `0${number}`;
+  }
+  return number;
+}
